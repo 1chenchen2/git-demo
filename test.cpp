c2b6ab6a27566c0b464aaf9079720957773ab86b
chenchen <chenchen@tztek.com>
2023-05-20 15:17:08 +0800

    测试代码

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+       freopen("test.in","w",stdout);
+       // 这里的意思是生成一个以.in结尾的文件，下面是文件的内容
+       for(int i=0;i<1e4;i++)
+        cout<<rand()%10000<<endl;//随机生成10000个数
+       return 0;
+}
